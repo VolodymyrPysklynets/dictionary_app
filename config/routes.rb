87250110cb739805451a67e6418ff1f2a11c4d5f
Vasyl Yatsyshyn <beketgit@gmail.com>
2023-12-07 19:56:09 +0200

    Add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/dashboard'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
   root "pages#dashboard"
 end
